54939c355cb7eecdd0bdfa4d27c3f90261d72f4d
melvin-mango <melvinlewisusiu@gmail.com>
2025-03-02 13:56:25 +0300

    Receptionist UI Complete

--- index.js
+++ index.js
@@ -15,7 +15,33 @@ app.get('/receptionist/dashboard', (request, response) =>{
     response.render('receptionist/dashboard');
 });
 
+app.get('/receptionist/assignrooms', (request, response) =>{
+    response.render('receptionist/assignrooms');
+});
 
+app.get('/receptionist/checkin', (request, response) =>{
+    response.render('receptionist/checkin');
+});
+
+app.get('/receptionist/checkout', (request, response) =>{
+    response.render('receptionist/checkout');
+});
+
+app.get('/receptionist/rooms', (request, response) =>{
+    response.render('receptionist/rooms');
+});
+
+app.get('/receptionist/roomstatus', (request, response) =>{
+    response.render('receptionist/roomstatus');
+});
+
+app.get('/receptionist/specialrequest', (request, response) =>{
+    response.render('receptionist/specialrequest');
+});
+
+app.get('/receptionist/booking', (request, response) =>{
+    response.render('receptionist/booking');
+});
 
 
 app.listen(port);
